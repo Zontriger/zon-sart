2957cd8852c94cbd908f033ee352173138dd6026
zontriger <zontriger@gmail.com>
2026-01-30 14:10:53 -0400

    Estructura de Base de datos terminada

--- db/sart_db.sql
+++ db/sart_db.sql
@@ -1,25 +1,42 @@
+-- Habilitar el soporte para claves foráneas
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE IF NOT EXISTS Usuario (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	name TEXT NOT NULL,
-	rol TEXT CHECK(rol IN ('admin', 'viewer'))
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT UNIQUE NOT NULL,
+    password TEXT NOT NULL,             -- Hasheado
+    full_name TEXT NOT NULL,
+    position TEXT,
+    rol TEXT CHECK(rol IN ('admin', 'viewer')) NOT NULL DEFAULT 'viewer'
 );
 
 CREATE TABLE IF NOT EXISTS Periodo (
-	code TEXT PRIMARY KEY,
-	fec_ini INTEGER NOT NULL, 
-	fec_end INTEGER NOT NULL,
-	CHECK(fec_ini < fec_end)
+    code TEXT PRIMARY KEY ,		-- Ej: "II-2025"
+    date_ini TEXT NOT NULL,		-- Tiempo Unix Timestamp
+    date_end TEXT NOT NULL,
+    is_current INTEGER DEFAULT 0 CHECK(is_current IN (0, 1)) -- 1 = Activo, 0 = Inactivo
 );
 
-CREATE TABLE IF NOT EXISTS Codigo_Dispositivo ();
-
-CREATE TABLE IF NOT EXISTS Dispositivo (
-	code TEXT PRIMARY KEY,
-	 TEXT
+CREATE TABLE IF NOT EXISTS Equipo (
+    code TEXT PRIMARY KEY,
+    area TEXT NOT NULL,
+    ubication TEXT NOT NULL,	-- Dónde está el equipo normalmente
+    os TEXT,
+    ram TEXT,
+    arch TEXT,
+    rom TEXT,
+    processor TEXT,
+    serial TEXT
 );
 
-CREATE TABLE IF NOT EXISTS Reporte (
-	id INTEGER PRIMARY KEY AUTOINCREMENT,
-	
-	status TEXT CHECK(status IN ('pending', 'repaired', 'unrepaired')),
+CREATE TABLE IF NOT EXISTS Taller (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    code_equip TEXT NOT NULL, 
+    status TEXT CHECK(status IN ('repaired', 'pending', 'unrepaired')) DEFAULT 'pending',
+    date_in TEXT NOT NULL,        -- Tiempo Unix Timestamp
+    date_out TEXT,                -- Puede ser NULL si el equipo sigue en taller
+    details TEXT,
+    
+    FOREIGN KEY (code_equip) REFERENCES Equipo(code)
+		ON DELETE NO ACTION ON UPDATE CASCADE
 );
